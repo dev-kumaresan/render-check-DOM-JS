4d069672023557db5754c4ac21e4675ededf98bb
Kumaresan L <100152824+dev-kumaresan@users.noreply.github.com>
2022-02-24 14:59:30 +0530

    Update render.js

--- js/render.js
+++ js/render.js
@@ -1,1 +1,13 @@
+function render()
+{
+    n = document.getElementById("img").value;
+    for(n;n>0;n--)
+    {
+        var image = document.createElement('img');
+        image.style.width='150px';
+        image.style.width = '150px';
+        image.setAttribute('src','../img/cat.jpg');
+        document.body.appendChild(image);
+    }
 
+}
