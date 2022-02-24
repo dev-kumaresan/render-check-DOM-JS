4c633aa34533cb21e49487e337d38ff44b75961b
Kumaresan L <100152824+dev-kumaresan@users.noreply.github.com>
2022-02-24 15:25:27 +0530

    Update render.js

--- js/render.js
+++ js/render.js
@@ -6,7 +6,7 @@ function render()
         var image = document.createElement('img');
         image.style.width='150px';
         image.style.width = '150px';
-        image.setAttribute('src','https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wiktionary.org%2Fwiki%2Fcat&psig=AOvVaw3mnD02svub7tRq8nry_FrJ&ust=1645778882191000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMD0_p_6l_YCFQAAAAAdAAAAABAD');
+        image.setAttribute('src','https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg');
         document.body.appendChild(image);
     }
 
